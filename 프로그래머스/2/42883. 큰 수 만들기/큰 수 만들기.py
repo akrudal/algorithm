b13a9949419c4f3be4c1a68f6918432063e996f2
g_mi <62610032+akrudal@users.noreply.github.com>
2024-03-21 19:10:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,25 @@
+def solution(number, k):
+    origin_k = k
+    stack = []
+    index = 0
+    
+    print("start", stack, index)
+    while(index < len(number) and k >= 0):
+        if not stack:
+            stack.append(number[index])
+            index += 1
+        
+        while(index < len(number) and stack[-1] >= number[index]):
+            stack.append(number[index])
+            index += 1
+
+        if index >= len(number) or k<= 0: break
+
+        while(k > 0 and stack and number[index] > stack[-1]):
+            stack.pop(-1)
+            k -= 1
+    
+    answer =  ('').join(stack) + number[index:]
+    if len(answer) > len(number) - origin_k:
+        answer = answer[:len(number)-origin_k]
+    return answer
