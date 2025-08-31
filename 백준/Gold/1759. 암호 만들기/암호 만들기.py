ad3650f6645082c0d018e450754d0b38ccf280d7
g_mi <62610032+akrudal@users.noreply.github.com>
2025-08-31 17:23:55 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,33 +1,39 @@
-r, n = map(int, input().split())
-n_list = sorted(list(input().split()))
+모음 = {"a", "e", "i", "o", "u"}
+stack = []
+alphabets = []
+# 서로 다른 L개, 최소 1개의 모음과 최소 두 개의 자음
 
-result = []
-
-vowels = ['a', 'e', 'i', 'o', 'u']
-vowels_count = 1
-consonants_count = 0
-
-def recursive(x):
-    global vowels_count, consonants_count
-    # print(result, vowels_count)
-    if len(result) == r and vowels_count <= 0 and consonants_count >= 2:
-        for c in result:
-            print(c, end = '')
-        print()
+def make_combinations(m, n, nindex):
+    length=len(stack)
+    
+    # print("current", nindex, stack, length, n)
+    
+    # 예외 조건: 배열의 인덱스를 넘을 경우는 제외
+    if nindex > m: return
+    # 종료 조건: C개 중 L만큼 뽑았을 때
+    if length == n:
+        # 모음이 1개라도 없을 경우는 제외
+        if (not (set(stack) & 모음) or (len(set(stack) - 모음) < 2)): 
+            return
+        
+        print("".join(stack))
         return
-    else:
-        for i in range(x, n):
-                result.append(n_list[i])
-                if n_list[i] in vowels:
-                    vowels_count -= 1
-                else:
-                    consonants_count +=1
-                # print(result, n_list[i], vowels_count)
-                recursive(i+1)
-                result.pop()
-                if n_list[i] in vowels:
-                    vowels_count += 1
-                else:
-                    consonants_count -= 1
+
+    for i, a in enumerate(alphabets[min(nindex, m-1): ]):
+        stack.append(a)
+        make_combinations(m, n, nindex+1)
+        stack.pop()
             
-recursive(0)
+        nindex += 1
+            
+def main():
+    global stack, alphabets
+    L, C = map(int, input().split())
+    alphabets = sorted(input().split())
+    
+    for i in range(C):
+        stack.append(alphabets[i])
+        make_combinations(C, L, i+1)
+        stack = []
+    
+main()
