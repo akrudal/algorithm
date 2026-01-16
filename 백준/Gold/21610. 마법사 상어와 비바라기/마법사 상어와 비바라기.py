26eb6091ee9cd7506b81c9ba9d1136f4a69fa0ba
g_mi <62610032+akrudal@users.noreply.github.com>
2026-01-16 20:05:24 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 376 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -2,38 +2,36 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-# 바구니
 A = [list(map(int, input().split())) for _ in range(N)]
-# 이동방향
 moves = [tuple(map(int, input().split())) for _ in range(M)]
 
-# 방향: 1~8 (방향 번호 그대로 쓰기 위해 0,0)
+# 방향 (1~8)
 dr = [0, 0, -1, -1, -1, 0, 1, 1, 1]
 dc = [0, -1, -1, 0, 1, 1, 1, 0, -1]
 
 # 초기 구름
 clouds = [(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)]
 
+# visited를 턴 번호로 재사용
+visited = [[0]*N for _ in range(N)]
+turn = 1
+
 for d, s in moves:
-    moved = [] # 
-    visited = [[False]*N for _ in range(N)]
+    new_clouds = []
 
-    # 1. 구름 이동
+    # 1. 구름 이동 + 2. 비 내리기
     for r, c in clouds:
-        nr = (r + dr[d]*s) % N # 경계를 초과할 경우
+        nr = (r + dr[d]*s) % N
         nc = (c + dc[d]*s) % N
-        moved.append((nr, nc))
+        A[nr][nc] += 1
+        visited[nr][nc] = turn
+        new_clouds.append((nr, nc))
 
-    # 2. 비 내리기
-    for r, c in moved:
-        A[r][c] += 1
-        visited[r][c] = True
-
-    # 3. 구름 제거
+    # 3. 구름 사라짐
     clouds = []
 
     # 4. 물복사버그
-    for r, c in moved:
+    for r, c in new_clouds:
         cnt = 0
         for drc, dcc in [(-1,-1), (-1,1), (1,-1), (1,1)]:
             nr, nc = r + drc, c + dcc
@@ -44,9 +42,11 @@ for d, s in moves:
     # 5. 새 구름 생성
     for i in range(N):
         for j in range(N):
-            if A[i][j] >= 2 and not visited[i][j]:
+            if A[i][j] >= 2 and visited[i][j] != turn:
                 clouds.append((i, j))
                 A[i][j] -= 2
 
-# 결과 출력
+    turn += 1
+
+# 결과
 print(sum(map(sum, A)))
